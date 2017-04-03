99e5d7d3750dbf9a51246874740136e7b2d6b092
aalexandraa <alex.proesl@gmail.com>
2017-04-03 16:34:56 +0200

    changed routes + updated products show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :products
+  get 'static_pages/index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   root 'static_pages#landing_page'
 
-
-  get 'static_pages/index'
+  get '/products/:id', to: 'products#show'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
