e53ac3dc3f1528ade5b7db77a4fd9a31b5fee7f7
aalexandraa <alex.proesl@gmail.com>
2017-04-06 15:26:04 +0200

    5.3 generate HTML emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)    @message = message
     mail(:from => email,
-        :to => 'your-email@example.com',
+        :to => 'alex.proesl@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
 end
