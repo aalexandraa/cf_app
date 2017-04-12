f0b340f6d2267f29d704bdbfc82986b195cd8527
aalexandraa <alex.proesl@gmail.com>
2017-04-12 18:23:30 +0200

    5.4 fix sign up email

---
+++ app/controllers/user_registration_controller.rb
@@ -0,0 +1,10 @@
+class UserRegistrationController < Devise::RegistrationsController
+
+  def create
+    super
+    if @user.persisted?
+      UserMailer.welcome(@user).deliver_now
+    end
+  end
+
+end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,10 @@ class UserMailer < ApplicationMailer
         :to => 'alex.proesl@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "Bike Berlin"
+    mail(:to => user.email, :subject => "Welcome to #{@appname}!")
+  end
+
 end
