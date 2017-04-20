46caac45c488ea0ac576dcfc2c096ae0628aaf18
aalexandraa <alex.proesl@gmail.com>
2017-04-20 17:08:00 +0200

    5.8 Comments Controller and Views
    + Major stylistic changes to clear columns
    + Created the controller and views for the product reviews
    + Implement star rating with Raty jQuery plugin, rather than looping in
    Ruby

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,9 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:user_id, :body, :rating)
+    end
 end
