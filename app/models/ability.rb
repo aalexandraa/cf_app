bbd69a502c99298daceb405a13dec45f2b4dae6c
aalexandraa <alex.proesl@gmail.com>
2017-04-21 16:39:23 +0200

    5.10 Admin Role & Moderating User Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, User, id: user.id
+      can [:index, :show, :search], Product
+      cannot [:create], Product
+    end
   end
-  
+
 end
