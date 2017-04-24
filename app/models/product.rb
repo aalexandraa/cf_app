0d4126947ba5c32fdc11ff1505f2ca8c8afb78e9
aalexandraa <alex.proesl@gmail.com>
2017-04-24 12:49:01 +0200

    add Validation to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ApplicationRecord
   has_many :orders
   has_many :comments
-  
+
   validates :name, presence: true
+  validates :price, numericality: true
+  validates :description, presence: true
+  validates :image_url, format: { with: %r{.(jpg|png)\Z}i, message: 'must be a URL for JPG or PNG image.' }
 
   def highest_rating_comment
     comments.rating_desc.first
