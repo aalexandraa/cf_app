ab37684e4d07685b78f720f3a73032916216a225
aalexandraa <alex.proesl@gmail.com>
2017-04-26 17:51:38 +0200

    6.3 add Functional Tests for users controller

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,12 +4,11 @@ require 'rails_helper'
 describe Product do
   before do
     #here you put your code to generate test content
-<<<<<<< HEAD
-    @product = Product.create!(name: "race bike", description: "Supexsr fast bike.", price: "299", image_url: "bestbike.jpg")
-=======
+
     @product = Product.create!(name: "race bike", description: "Super fast bike.", price: "299", image_url: "bestbike.jpg")
->>>>>>> 3439b9509f01ffdf8233a970ca8345fa75891a87
+
     @user = User.create!(email: "tardarsauce@grumpy.com", password: "GrumpyCat191")
+
     @product.comments.create!(rating: 1, user: @user, body: "Meow!")
     @product.comments.create!(rating: 3, user: @user, body: "Meow Meow Meow!")
     @product.comments.create!(rating: 5, user: @user, body: "Meow Meow Meow Meow Meow!")
