273f1f8d262faee36ebce4fdd9477d64703edefa
aalexandraa <alex.proesl@gmail.com>
2017-05-09 17:51:06 +0200

    6.6 Creating Payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,31 @@
 class PaymentsController < ApplicationController
-def create
-  @product = Product.find(params[:product_id])
-  @user = current_user
-  token = params[:stripeToken]
-  # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      :amount => @product.price * 100
-      :currency => "eur",
-      :source => token,
-      :description => params[:stripeEmail]
-    )
-
-    if charge.paid
-      Order.create(
-      product_id: @product.id,
-      user_id: @user.id,
-      total: @product.price * 100
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => @product.price * 100
+        :currency => "eur",
+        :source => token,
+        :description => params[:stripeEmail]
       )
-    end
 
-  rescue Stripe::CardError => e
-    # The card has been declined
-    body = e.json_body
-    err = body[:error]
-    flash[:error] = "Unfortunatly, there was an error procession your payment: #{err[:message]}"
+      if charge.paid
+        Order.create(
+        product_id: @product.id,
+        user_id: @user.id,
+        total: @product.price * 100
+        )
+      end
+
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunatly, there was an error procession your payment: #{err[:message]}"
+    end
+    redirect_to product_path(@product)
   end
-  redirect_to product_path(@product)
 end
