dd0786fa94b4d371445e63e47fc9902e7aad80e2
aalexandraa <alex.proesl@gmail.com>
2017-05-23 12:54:00 +0200

    6.13 Real-Time Updates with ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'https://bike-berlin-ap.herokuapp.com' }
+
+  config.web_socket_server_url = "wss://bike-berlin-ap.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://bike-berlin-ap.herokuapp.com', 'http://bike-berlin-ap.herokuapp.com']
 end
